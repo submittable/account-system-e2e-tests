2444ab050c29bb849ef7d9d6167e341a2bf1e3b1
Brad Deibert <brad.deibert@submittable.com>
2024-07-02 14:45:14 -0600

    upd8 error msg

--- utils/env-helper.mjs
+++ utils/env-helper.mjs
@@ -3,7 +3,7 @@ import 'dotenv/config';
 const getEnvVar = (varName, isRequired = true) => {
   if (!process.env[varName])
   {
-    console.error(`Environment variable ${varName} not found!`);
+    console.error(`Value not found for environment variable '${varName}'`);
     
     // only exit when env var is required
     if (isRequired) {
