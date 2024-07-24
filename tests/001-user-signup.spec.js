431d05935d9e6674a82e670ded0833500e883e3f
Brad Deibert <brad.deibert@submittable.com>
2024-07-24 13:23:16 -0600

    wrap cleanup logic into function - call upon failure retries

--- tests/001-user-signup.spec.js
+++ tests/001-user-signup.spec.js
@@ -1,10 +1,16 @@
 import { test, expect } from '@playwright/test'
 import { getLatestEmailMessage, getEmailById, getEmailBody, mailinatorDomain } from '../utils/mailinator-client.mjs'
+import { cleanup } from '../utils/cleanup.mjs';
 
 const demoAppUrl = process.env.ACCOUNTS_DEMO_APP_URL;
 
 test.describe("Sign-up flow", () => {
-  test("Successfully sign up for account", async ({ page, browserName }) => {
+  test("Successfully sign up for account", async ({ page, browserName }, testInfo) => {
+    // clear out test user state if retry
+    if (testInfo.retry) {
+      await cleanup();
+    }
+
     // navigate to Auth0 signup page through demo app
     await page.goto(demoAppUrl);
     await page.getByText('Sign Up', { exact: true }).click();
